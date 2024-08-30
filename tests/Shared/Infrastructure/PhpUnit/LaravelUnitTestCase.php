ebcbee1b93e9c6fe7fbf981ea9376b72223f4ed4
Victor Planchuelo <victor.planchuelo@motocard.com>
2024-08-30 13:45:33 +0200

    fix(main): Remove final class to LaravelUnitTestCase

--- tests/Shared/Infrastructure/PhpUnit/LaravelUnitTestCase.php
+++ tests/Shared/Infrastructure/PhpUnit/LaravelUnitTestCase.php
@@ -7,7 +7,7 @@ namespace Tests\Shared\Infrastructure\PhpUnit;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Tests\Shared\TestCase;
 
-final class LaravelUnitTestCase extends TestCase
+class LaravelUnitTestCase extends TestCase
 {
 	use MockeryPHPUnitIntegration;
 }
